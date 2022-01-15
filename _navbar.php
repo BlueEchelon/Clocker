c6f5ca71d30b810ce34904912eda8362390757f8
Adrian Tworek <62119460+AdrianTworek@users.noreply.github.com>
2022-01-15 18:22:54 +0100

    Add files via upload

---
+++ _navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar">
+  <div class="navbar__container">
+    <a href="index.php">
+      <div class="navbar__heading">
+        <h1>Clocker</h1>
+        <img src="../images//logo.svg" alt="Clocker logo" />
+      </div>
+    </a>
+
+    <ul class="navbar__list">
+      <li class="navbar__list-item">
+        <a href="#">Home</a>
+      </li>
+      <li class="navbar__list-item">
+        <a href="#">About us</a>
+      </li>
+      <li class="navbar__list-item">
+        <a href="#">Statistics</a>
+      </li>
+    </ul>
+
+    <div class="navbar__buttons-container">
+      <a class="btn-primary" href="login.php">Sign in</a>
+      <a class="btn-primary btn-primary--filled" href="register.php">Sign
+        up</a>
+    </div>
+  </div>
+</nav>
