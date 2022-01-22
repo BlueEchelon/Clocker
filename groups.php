7d5e787b3e21c95aa2fb28c9dc47e2d6d34073b3
Przemek Nosek <przemek.nosek@o2.pl>
2022-01-22 11:56:02 +0100

    wypisanie grup dla zalogowanego uzytkownika

--- groups.php
+++ groups.php
@@ -2,7 +2,15 @@
 <html lang="en">
 
 <?php require_once "_head.php" ?>
+<?php
+session_start();
 
+if(!isset($_SESSION['ID']))
+{
+    header('Location: index.php');
+    exit();
+}
+?>
 <body>
   <?php require_once "_navbar_main.php" ?>
 
@@ -26,25 +34,53 @@
           <span>Title</span>
           <span>Number of members</span>
         </div>
+          <?php
+          require_once "connect.php";
 
-        <div class="projects__row-container">
-          <span>Group 1</span>
-          <span>69</span>
-          <a href="group_details.php"
-            class="details btn-primary--filled">Details</a>
-        </div>
-        <div class="projects__row-container">
-          <span>Group 2</span>
-          <span>69</span>
-          <a href="group_details.php"
-            class="details btn-primary--filled">Details</a>
-        </div>
-        <div class="projects__row-container">
-          <span>Group 3</span>
-          <span>69</span>
-          <a href="group_details.php"
-            class="details btn-primary--filled">Details</a>
-        </div>
+          $id = $_SESSION['ID'];
+          $sql = "select * from clocker_db.groups g, members m where g.ID=m.group_id and user_id=:id";
+          $handle = $pdo->prepare($sql);
+          $params = ['id' => $id];
+          $handle->execute($params);
+          if ($handle->rowCount() > 0) {
+              while ($getRow = $handle->fetch(PDO::FETCH_ASSOC)) {
+                  $name = $getRow['name'];
+                  $group_id = $getRow['group_id'];
+
+                  $prepare = "select count(*) as Licznik from clocker_db.groups g,
+                     members m where g.ID=m.group_id and g.ID=:group_id";
+                  $hand = $pdo->prepare($prepare);
+                  $param = ['group_id' => $group_id];
+                  $hand->execute($param);
+                  $counter = $hand->fetch(PDO::FETCH_ASSOC);
+
+                  echo '<div class="projects__row-container">
+                                  <span>' . $name . '</span>
+                                  <span>' . $counter['Licznik'] . '</span>
+                                  <a href="groups_details.php?id=' . $group_id . '"
+                                    class="details btn-primary--filled">Details</a>
+                                  </div>';
+              }
+          }
+          ?>
+<!--        <div class="projects__row-container">-->
+<!--          <span>Group 1</span>-->
+<!--          <span>69</span>-->
+<!--          <a href="group_details.php"-->
+<!--            class="details btn-primary--filled">Details</a>-->
+<!--        </div>-->
+<!--        <div class="projects__row-container">-->
+<!--          <span>Group 2</span>-->
+<!--          <span>69</span>-->
+<!--          <a href="group_details.php"-->
+<!--            class="details btn-primary--filled">Details</a>-->
+<!--        </div>-->
+<!--        <div class="projects__row-container">-->
+<!--          <span>Group 3</span>-->
+<!--          <span>69</span>-->
+<!--          <a href="group_details.php"-->
+<!--            class="details btn-primary--filled">Details</a>-->
+<!--        </div>-->
 
       </div>
     </div>
--- projects.php
+++ projects.php
@@ -7,10 +7,10 @@ function debug_to_console($data)
     if (is_array($output)) $output = implode(',', $output);
     echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
 }
+
 session_start();
 unset($_SESSION['p_id']);
-if(!isset($_SESSION['ID']))
-{
+if (!isset($_SESSION['ID'])) {
     header('Location: index.php');
     exit();
 }
@@ -23,19 +23,19 @@ if (isset($_POST['Add'])) {
         $handle = $pdo->prepare($sql);
         $params = ['client' => $client];
         $handle->execute($params);
-        if ($handle->rowCount()>0) {
-            while($getRow = $handle->fetch(PDO::FETCH_ASSOC)){
-                if($getRow['user_id']==$_SESSION['ID']){
-                    $client_id=$getRow['ID'];
+        if ($handle->rowCount() > 0) {
+            while ($getRow = $handle->fetch(PDO::FETCH_ASSOC)) {
+                if ($getRow['user_id'] == $_SESSION['ID']) {
+                    $client_id = $getRow['ID'];
                     $sql = "INSERT INTO projects (user_id,client_id,name) values (:userId, :clientId, :p_name)";
-                    try{
+                    try {
                         $handle = $pdo->prepare($sql);
                         $params = [
-                                ':userId' =>$_SESSION['ID'],
-                                ':clientId' => $client_id,
-                                ':p_name' => $p_name,
-                                ];
-                                $handle->execute($params);
+                            ':userId' => $_SESSION['ID'],
+                            ':clientId' => $client_id,
+                            ':p_name' => $p_name,
+                        ];
+                        $handle->execute($params);
                     } catch (PDOException $e) {
                         $errors[] = $e->getMessage();
                     }
@@ -46,12 +46,12 @@ if (isset($_POST['Add'])) {
                     $errors[] = 'Email address already registered';
                 }
             }
-        }else{
+        } else {
             $sql = "INSERT INTO clients (name,user_id) values (:c_name,:userId)";
-            try{
+            try {
                 $handle = $pdo->prepare($sql);
                 $params = [
-                    ':c_name' =>$client,
+                    ':c_name' => $client,
                     ':userId' => $_SESSION['ID'],
                 ];
                 $handle->execute($params);
@@ -62,15 +62,15 @@ if (isset($_POST['Add'])) {
             $handle = $pdo->prepare($sql);
             $params = ['client' => $client];
             $handle->execute($params);
-            if ($handle->rowCount()>0) {
-                while($getRow = $handle->fetch(PDO::FETCH_ASSOC)){
-                    if($getRow['user_id']==$_SESSION['ID']){
-                        $client_id=$getRow['ID'];
+            if ($handle->rowCount() > 0) {
+                while ($getRow = $handle->fetch(PDO::FETCH_ASSOC)) {
+                    if ($getRow['user_id'] == $_SESSION['ID']) {
+                        $client_id = $getRow['ID'];
                         $sql = "INSERT INTO projects (user_id,client_id,name) values (:userId, :clientId, :p_name)";
-                        try{
+                        try {
                             $handle = $pdo->prepare($sql);
                             $params = [
-                                ':userId' =>$_SESSION['ID'],
+                                ':userId' => $_SESSION['ID'],
                                 ':clientId' => $client_id,
                                 ':p_name' => $p_name,
                             ];
@@ -86,62 +86,60 @@ if (isset($_POST['Add'])) {
                 }
             }
         }
-            /*
-            $sql = "INSERT INTO projects (email, password, name, surname, role) values (:email, :passw, :fname, :sur, :urole)";
-            try {
-                $handle = $pdo->prepare($sql);
-                $params = [
-                        ':p_name' => $email,
-                        ':Client' => $hashPassword,
-                        ':u_id' =>$_SESSION['ID'],
-                        ];
-                        $handle->execute($params);
-                        $success = 'User has been created successfully;';
-                    } catch (PDOException $e) {
-                        $errors[] = $e->getMessage();
-                    }
-                } else {
-                    $valName = $name;
-                    $valSurName = $surname;
-                    $valEmail = '';
-                    $valPassword = $password;
-                    $valConPassword = $confirmPass;
-
-                    $errors[] = 'Email address already registered';
+        /*
+        $sql = "INSERT INTO projects (email, password, name, surname, role) values (:email, :passw, :fname, :sur, :urole)";
+        try {
+            $handle = $pdo->prepare($sql);
+            $params = [
+                    ':p_name' => $email,
+                    ':Client' => $hashPassword,
+                    ':u_id' =>$_SESSION['ID'],
+                    ];
+                    $handle->execute($params);
+                    $success = 'User has been created successfully;';
+                } catch (PDOException $e) {
+                    $errors[] = $e->getMessage();
                 }
-            } else $errors[] = "Email address is not valid";
+            } else {
+                $valName = $name;
+                $valSurName = $surname;
+                $valEmail = '';
+                $valPassword = $password;
+                $valConPassword = $confirmPass;
+
+                $errors[] = 'Email address already registered';
+            }
+        } else $errors[] = "Email address is not valid";
+} else {
+    if (!isset($_POST['name']) || empty($_POST['name'])) {
+        $errors[] = 'First name is required';
     } else {
-        if (!isset($_POST['name']) || empty($_POST['name'])) {
-            $errors[] = 'First name is required';
-        } else {
-            $valName = $_POST['name'];
-        }
-        if (!isset($_POST['surname']) || empty($_POST['surname'])) {
-            $errors[] = 'Surname is required';
-        } else {
-            $valSurName = $_POST['surname'];
-        }
+        $valName = $_POST['name'];
+    }
+    if (!isset($_POST['surname']) || empty($_POST['surname'])) {
+        $errors[] = 'Surname is required';
+    } else {
+        $valSurName = $_POST['surname'];
+    }
 
-        if (!isset($_POST['email']) || empty($_POST['email'])) {
-            $errors[] = 'Email is required';
-        } else {
-            $valEmail = $_POST['email'];
-        }
+    if (!isset($_POST['email']) || empty($_POST['email'])) {
+        $errors[] = 'Email is required';
+    } else {
+        $valEmail = $_POST['email'];
+    }
 
-        if (!isset($_POST['password']) || empty($_POST['password'])) {
-            $errors[] = 'Password is required';
-        } else {
-            $valPassword = $_POST['password'];
-        }
-        if (!isset($_POST['confirmPass']) || empty($_POST['confirmPass'])) {
-            $errors[] = 'Password is required';
-        } else {
-            $valConPassword = $_POST['confirmPass'];
-        }*/
+    if (!isset($_POST['password']) || empty($_POST['password'])) {
+        $errors[] = 'Password is required';
+    } else {
+        $valPassword = $_POST['password'];
+    }
+    if (!isset($_POST['confirmPass']) || empty($_POST['confirmPass'])) {
+        $errors[] = 'Password is required';
+    } else {
+        $valConPassword = $_POST['confirmPass'];
+    }*/
     }
 }
-
-
 
 
 ?>
@@ -151,12 +149,12 @@ if (isset($_POST['Add'])) {
 <?php require_once "_navbar_main.php" ?>
 
 <div class="container">
-    <form class="add-project__container"  method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <input class="form__input"  type="text" name="project_name" placeholder="Project name"
-               value="<?php echo (isset($valName) ? $valName : '') ?>">
-        <input class="form__input"  type="text" name="client" placeholder="Client"
-               value="<?php echo (isset($valClient) ? $valClient : '') ?>">
-        <input type="submit"  name="Add" value="Add project"
+    <form class="add-project__container" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input class="form__input" type="text" name="project_name" placeholder="Project name"
+               value="<?php echo(isset($valName) ? $valName : '') ?>">
+        <input class="form__input" type="text" name="client" placeholder="Client"
+               value="<?php echo(isset($valClient) ? $valClient : '') ?>">
+        <input type="submit" name="Add" value="Add project"
                class="form__btn btn-primary btn-primary--filled"/>
     </form>
 
@@ -182,17 +180,17 @@ ORDER BY p.ID DESC";
             $handle = $pdo->prepare($sql);
             $params = ['id' => $id];
             $handle->execute($params);
-            if ($handle->rowCount()>0){
-                while ($getRow = $handle->fetch(PDO::FETCH_ASSOC)){
+            if ($handle->rowCount() > 0) {
+                while ($getRow = $handle->fetch(PDO::FETCH_ASSOC)) {
                     $name = $getRow['name'];
                     $client = $getRow['Klient'];
                     $counter = $getRow['Licznik'];
                     $p_id = $getRow['ID'];
                     echo '<div class="projects__row-container">
-                                  <span>'.$name.'</span>
-                                  <span>'.$client.'</span>
-                                  <span>'.$counter.'</span>
-                                  <a href="project_details.php?id='.$p_id.'"
+                                  <span>' . $name . '</span>
+                                  <span>' . $client . '</span>
+                                  <span>' . $counter . '</span>
+                                  <a href="project_details.php?id=' . $p_id . '"
                                     class="details btn-primary--filled">Details</a>
                                   </div>';
                 }
