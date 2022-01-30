69ec6e0a5138f31a98c163cf071e33085f68c1dc
Adrian Tworek <62119460+AdrianTworek@users.noreply.github.com>
2022-01-30 19:37:04 +0100

    Zmiana klas

--- project_details.php
+++ project_details.php
@@ -10,15 +10,14 @@ function debug_to_console($data)
     echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
 }
 session_start();
-if (!isset($_SESSION['p_id'])){
-    $_SESSION['p_id']=$_GET['P_id'];
+if (!isset($_SESSION['p_id'])) {
+    $_SESSION['p_id'] = $_GET['P_id'];
 }
-if(!isset($_SESSION['ID']))
-{
+if (!isset($_SESSION['ID'])) {
     header('Location: index.php');
     exit();
 }
-$records=array();
+$records = array();
 require_once "connect.php";
 
 $id = $_SESSION['p_id'];
@@ -26,16 +25,15 @@ $sql = "SELECT p.ID FROM projects as p WHERE p.ID=:id ";
 $handle = $pdo->prepare($sql);
 $params = ['id' => $id];
 $handle->execute($params);
-if($handle->rowCount()>0){
+if ($handle->rowCount() > 0) {
     $getRow = $handle->fetch(PDO::FETCH_ASSOC);
-    $p_id=$getRow['ID'];
-    if($p_id!=$_SESSION['p_id']){
+    $p_id = $getRow['ID'];
+    if ($p_id != $_SESSION['p_id']) {
         unset($_SESSION['p_id']);
         header('Location: projects.php');
         exit();
     }
-}
-else{
+} else {
     unset($_SESSION['p_id']);
     header('Location: projects.php');
     exit();
@@ -49,15 +47,15 @@ if (isset($_POST['Add'])) { // dodawanie tasków
         try {
             $handle = $pdo->prepare($sql);
             $params = [
-                'projectId'=>$_SESSION['p_id'],
+                'projectId' => $_SESSION['p_id'],
                 't_name' => $t_name,
                 'description' => $description
             ];
             $handle->execute($params);
-        }catch (PDOException $e) {
+        } catch (PDOException $e) {
             $errors[] = $e->getMessage();
         }
-    }else {
+    } else {
         if (!isset($_POST['task_name']) || empty($_POST['task_name'])) {
             $errors[] = 'Task name is required';
         } else {
@@ -72,14 +70,14 @@ if (isset($_POST['button'])) { //przyciski STOP/START z update'em do bazy timest
     $handle = $pdo->prepare($sql);
     $params = ['id' => $t_id];
     $handle->execute($params);
-    if($handle->rowCount()>0){
+    if ($handle->rowCount() > 0) {
         $getRow = $handle->fetch(PDO::FETCH_ASSOC);
-        if ($getRow['status']==0){
+        if ($getRow['status'] == 0) {
             $updt = "update tasks set start=now(),status=1 WHERE ID=:id";
             $handle = $pdo->prepare($updt);
             $params = ['id' => $t_id];
             $handle->execute($params);
-        }else{
+        } else {
             $updt = "update tasks set stop=now() WHERE ID=:id";
             $handle = $pdo->prepare($updt);
             $params = ['id' => $t_id];
@@ -90,11 +88,11 @@ if (isset($_POST['button'])) { //przyciski STOP/START z update'em do bazy timest
             $params = ['id' => $t_id];
             $handle->execute($params);
             $getRow = $handle->fetch(PDO::FETCH_ASSOC);
-            $start=strtotime($getRow['start']);
-            $stop=strtotime($getRow['stop']);
-            $timer=strtotime($getRow['timer']);
-            $past=$stop-$start + $timer;
-            $time=date("H:i:s",$past);
+            $start = strtotime($getRow['start']);
+            $stop = strtotime($getRow['stop']);
+            $timer = strtotime($getRow['timer']);
+            $past = $stop - $start + $timer;
+            $time = date("H:i:s", $past);
 
             $updt = "update tasks set status=0 , timer=:time WHERE ID=:id";
             $handle = $pdo->prepare($updt);
@@ -103,7 +101,6 @@ if (isset($_POST['button'])) { //przyciski STOP/START z update'em do bazy timest
                 'time' => $time
             ];
             $handle->execute($params);
-
         }
     }
 }
@@ -120,10 +117,10 @@ if (isset($_POST['submit_task'])) { // zmiana opisu taska
                 'upd_desc' => $description
             ];
             $handle->execute($params);
-        }catch (PDOException $e) {
+        } catch (PDOException $e) {
             $errors[] = $e->getMessage();
         }
-    }else {
+    } else {
         if (!isset($_POST['edit-task']) || empty($_POST['edit-task'])) {
             $errors[] = 'Task name is required';
         } else {
@@ -140,37 +137,40 @@ if (isset($_POST['submit_task'])) { // zmiana opisu taska
   <div class="container">
     <div>
       <h2>
-          <?php
-          require_once "connect.php";
-          $id = $_SESSION['p_id'];
-          $sql = "SELECT p.name   FROM projects as p  WHERE p.ID=:id  ";
-          $handle = $pdo->prepare($sql);
-          $params = ['id' => $id];
-          $handle->execute($params);
-          if($handle->rowCount()>0) {
-              $getRow = $handle->fetch(PDO::FETCH_ASSOC);
-              $name = $getRow['name'];
-              echo $name . " - " . $_SESSION['name'];
-          }
-          //$today = date("Y-m-d H:i:s"); format timestampa
-          //echo $date = date('Y-m-d H:i:s');
-      ?></h2>
+        <?php
+                require_once "connect.php";
+                $id = $_SESSION['p_id'];
+                $sql = "SELECT p.name   FROM projects as p  WHERE p.ID=:id  ";
+                $handle = $pdo->prepare($sql);
+                $params = ['id' => $id];
+                $handle->execute($params);
+                if ($handle->rowCount() > 0) {
+                    $getRow = $handle->fetch(PDO::FETCH_ASSOC);
+                    $name = $getRow['name'];
+                    echo $name . " - " . $_SESSION['name'];
+                }
+                //$today = date("Y-m-d H:i:s"); format timestampa
+                //echo $date = date('Y-m-d H:i:s');
+                ?></h2>
     </div>
 
-    <form class="add-project__container" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-      <input class="form__input" type="text" name="task_name" placeholder="Task name"
-             value="<?php echo (isset($valName) ? $valName : '') ?>">
-        <input class="form__input" type="text" name="description" placeholder="Description"
-               value="<?php echo(isset($valName) ? $valName : '') ?>">
+    <form class="add-project__container" method="post"
+      action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      <input class="form__input" type="text" name="task_name"
+        placeholder="Task name"
+        value="<?php echo (isset($valName) ? $valName : '') ?>">
+      <input class="form__input" type="text" name="description"
+        placeholder="Description"
+        value="<?php echo (isset($valName) ? $valName : '') ?>">
       <input type="submit" name="Add" value="Add task"
         class="form__btn btn-primary btn-primary--filled" />
     </form>
-      <?php if (isset($errors) && count($errors) > 0) {
-          foreach ($errors as $error_msg) {
-              echo '<div style="color: red;">' . $error_msg . '</div>';
-          }
-      }
-      ?>
+    <?php if (isset($errors) && count($errors) > 0) {
+            foreach ($errors as $error_msg) {
+                echo '<div style="color: red;">' . $error_msg . '</div>';
+            }
+        }
+        ?>
 
     <div class="projects">
       <h3 class="projects__title">Tasks</h3>
@@ -182,64 +182,67 @@ if (isset($_POST['submit_task'])) { // zmiana opisu taska
           <span>Description</span>
         </div>
 
-          <?php
-          require_once "connect.php";
+        <?php
+                require_once "connect.php";
 
-          $id = $_SESSION['p_id'];
-          $sql = "SELECT t.ID,t.name, t.timer, t.description, t.status FROM tasks as t WHERE t.project_id=:id ORDER BY t.ID DESC";
-          $handle = $pdo->prepare($sql);
-          $params = ['id' => $id];
-          $handle->execute($params);
-          if($handle->rowCount()>0){
-              while($getRow = $handle->fetch(PDO::FETCH_ASSOC)){
-                  $name=$getRow['name'];
-                  $desc=$getRow['description'];
-                  $timer=strtotime($getRow['timer']);
-                  $timer=date("H:i:s",$timer);
-                  if($getRow['status']==1){
-                      $status = 'STOP';
-                      $classa='class="not-working"';
-                  }
-                  else {
-                      $status = 'START';
-                      $classa='class="working"';
-                  }
-                  $t_id=$getRow['ID'];
-                  echo '<form class="projects__row-container" method="post" action="'.$_SERVER['PHP_SELF'].'">
-                           <input name="task_id" style="display: none" value="'.$t_id.'">
-                          <span id="project-name"  >'.$name.'</span>
-                          <span '.$classa.' id="clock">'.$timer.'</span>
-                          <span id="project-name">'.$desc.'</span>
-                          <input type="submit" name ="button" class="details btn-primary--filled"  value="'.$status.'"/>
+                $id = $_SESSION['p_id'];
+                $sql = "SELECT t.ID,t.name, t.timer, t.description, t.status FROM tasks as t WHERE t.project_id=:id ORDER BY t.ID DESC";
+                $handle = $pdo->prepare($sql);
+                $params = ['id' => $id];
+                $handle->execute($params);
+                if ($handle->rowCount() > 0) {
+                    while ($getRow = $handle->fetch(PDO::FETCH_ASSOC)) {
+                        $name = $getRow['name'];
+                        $desc = $getRow['description'];
+                        $timerTmp = strtotime($getRow['timer']);
+                        $timer = date("H:i:s", $timerTmp);
+                        if ($getRow['status'] == 1) {
+                            $status = 'STOP';
+                            $classa = 'class="timer working"';
+                        } else {
+                            $status = 'START';
+                            $classa = 'class="timer not-working"';
+                        }
+                        $t_id = $getRow['ID'];
+                        echo '<form class="projects__row-container" method="post" action="' . $_SERVER['PHP_SELF'] . '">
+                           <input name="task_id" style="display: none" value="' . $t_id . '">
+                          <span id="project-name"  >' . $name . '</span>
+                          <span ' . $classa . ' id="clock"' . $t_id . '">' . $timer . '</span>
+                          <span id="project-name">' . $desc . '</span>
+                          <input type="submit" name ="button"
+                            class="details btn-primary--filled"  value="' . $status . '"/>
                         </form>';
-              }
-          }
-          ?>
+                    }
+                }
+                ?>
       </div>
     </div>
-    <form class="add-project__container" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <select class="form__input" name="edit-task" type="text" placeholder="Task name">
-            <?php
-            require_once "connect.php";
-            $id = $_SESSION['p_id'];
-            $sql = "select name,ID from tasks where project_id=:id";
-            $handle = $pdo->prepare($sql);
-            $params = ['id' => $id];
-            $handle->execute($params);
-            if ($handle->rowCount() > 0) {
-                while ($getRow = $handle->fetch(PDO::FETCH_ASSOC)) {
-                    $name = $getRow['name'];
-                    $t_id=$getRow['ID'];
-                    echo '<option value=' . $t_id . '>' . $name . '</option>';
+    <form class="add-project__container" method="POST"
+      action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      <select class="form__input" name="edit-task" type="text"
+        placeholder="Task name">
+        <?php
+                require_once "connect.php";
+                $id = $_SESSION['p_id'];
+                $sql = "select name,ID from tasks where project_id=:id";
+                $handle = $pdo->prepare($sql);
+                $params = ['id' => $id];
+                $handle->execute($params);
+                if ($handle->rowCount() > 0) {
+                    while ($getRow = $handle->fetch(PDO::FETCH_ASSOC)) {
+                        $name = $getRow['name'];
+                        $t_id = $getRow['ID'];
+                        echo '<option value=' . $t_id . '>' . $name . '</option>';
+                    }
                 }
-            }
-            ?>
-            <!--            <option value="Project 1">Project 1</option>-->
-            <!--            <option value="Project 2">Project 2</option>-->
-            <!--            <option value="Project 3">Project 3</option>-->
-        </select>
-        <input class="form__input" type="text" name="description" placeholder="Description"
-               value="<?php echo(isset($valDesc) ? $valDesc : '') ?>">
+                ?>
+        <!--            <option value="Project 1">Project 1</option>-->
+        <!--            <option value="Project 2">Project 2</option>-->
+        <!--            <option value="Project 3">Project 3</option>-->
+      </select>
+      <input class="form__input" type="text" name="description"
+        placeholder="Description"
+        value="<?php echo (isset($valDesc) ? $valDesc : '') ?>">
       <input type="submit" name="submit_task" value="Edit task"
         class="form__btn btn-primary btn-primary--filled" />
 
